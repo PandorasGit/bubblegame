40a7a5b571fd9d653ab0413ee4c071fd051d7699
Roberts <waroberts@bsu.edu>
2025-01-24 20:20:16 -0500

    Add level transition system

--- bubble/level/level.gd
+++ bubble/level/level.gd
@@ -1,7 +1,17 @@
 extends Node2D
 
+@export var next_level : String
 var score := 0
+var goal_score : int
+
+func _ready() -> void:
+	goal_score = $Blocks.get_child_count()
 
 func _on_area_2d_block_entered() -> void:
 	score += 1
 	$Label.text = "Score:" + str(score)
+	if score >= goal_score:
+		transition()
+
+func transition():
+	get_tree().call_deferred("change_scene_to_file",next_level)
