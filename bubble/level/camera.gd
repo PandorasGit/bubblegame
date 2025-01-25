a5ae444ccf8bd49d0598f6fc3dd91006f5ccc4a6
Roberts <waroberts@bsu.edu>
2025-01-25 13:41:36 -0500

    Add levels

--- bubble/level/camera.gd
+++ bubble/level/camera.gd
@@ -3,14 +3,16 @@ extends Camera2D
 var target : RigidBody2D
 var track = true
 var default_position = global_position
+var target_last_position
 
 func _on_launched(bubble):
 	track = true
 	target = bubble
 	
 func _physics_process(delta: float) -> void:
-	if position.y >= 800:
-		track = false
+	if target:
+		if position.y >= 800 or target.sleeping:
+			track = false
 	
 	if track and target:
 		lerp
